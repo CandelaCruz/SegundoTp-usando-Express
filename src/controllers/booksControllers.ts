53fd32228b4cd71f17b6636ff07f20701478a683
Cande <candelamagalicruz2628@gmail.com>
2025-06-01 20:00:24 -0300

    Creo un libro

--- src/controllers/booksControllers.ts
+++ src/controllers/booksControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express" 
 import { Book } from "../models/booksModel"
 
+//OBTENER TODOS LOS LIBROS
 const getAllBooks = async (req: Request, res: Response): Promise<any> => {
   try {
     const books = await Book.find()
@@ -18,10 +19,26 @@ const getAllBooks = async (req: Request, res: Response): Promise<any> => {
   }
 }
 
+//OBTENER UN LIBRO POR ID
+ const getBooksById = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ success: false, message: "Book not found" });
 
-const getBooksById = async () => {}
+    res.json({ success: true, data: book });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
 
-const createBooks = async () =>{}  
+//CREAR UN LIBRO
+const createBooks = async (req: Request, res: Response): Promise<any> => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}  
 
 const updateBook = async () => {}
 
